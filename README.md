e030dfd9520b6110bc5fd98140a4e33818d1e9ae
Freewind <nowindlee@gmail.com>
2020-08-04 00:37:58 +0800

    typescript-react-useeffect-on-undefined-demo

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 TypeScript React Hello World Demo
 =================================
 
+我忘了之前是在什么情况下得出了一个结论：当useEffect中的某个dependency为undefined的时候（比如是从props中得到，而该dependency的值为optional且未赋值），
+会导致useEffect总是被调用。不过在这个demo里试了一下，发现不存在这种情况，当时可能是别的原因弄错了。
+
 ```
 npm install
 npm run demo
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,8 +1,18 @@
-//import * as React from 'react'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
+function Counter({count}: { count?: number }) {
+  useEffect(() => {
+    console.log("### useEffect", count);
+  }, [count])
+  return <div>Count: {count}</div>
+}
 
 export default function Hello() {
+  const [version, setVersion] = useState(0);
+
   return <div>
-    <h1>Hello React</h1>
+    <h1>{JSON.stringify(version)}</h1>
+    <button onClick={() => setVersion(v => v + 1)}>Force Reload</button>
+    <Counter/>
   </div>
 };
